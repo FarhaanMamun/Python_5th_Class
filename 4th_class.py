58287cb6dbf9986aa0ab53fa732b9dd193192264
unknown <farhaanmamu0087@gmail.com>
2022-05-20 23:26:02 +0600

    Python

---
+++ 4th_class.py
@@ -0,0 +1,44 @@
+name= "hi! there timmy"
+print(name[3:8])
+print(name[3:8:2])
+string ="121" # *12345"
+bool = (string == string[::-1])
+print(bool)
+str  = "Hi!I am there"
+print(len(str))
+#remove white space(left)
+str  = " Hi!I am there"
+print(str.strip())
+str  = "Hi!I am there"
+print(str.strip("Hi"))
+#spliting into list
+str  = "Hi!I am there"
+print(str.split("Hi"))
+#replace by other words
+str  = "Hi!I am there"
+print(str.replace("Hi","Hlw"))
+print(str.replace("there","Here"))
+#For Upper Case
+str  = "Hi!I am there"
+print(str.upper())
+#For First letters upper case
+str  = "Hi! I am there"
+print(str.capitalize)
+#sentence startswith ,endswith
+str  = "Hi! I am there"
+print(str.startswith("Hi!"))
+print(str.startswith("n"))
+print(str.endswith("e"))
+#index position of where it was found
+str  = "Hi! I am there"
+print(str.index("e")) #count the first e
+#count,Can count the specific character
+str  = "Hi! I am there"
+print(str.count("e"))
+#find the index of character
+
+
+#bool
+#print(bool(None))
+#print(bool(None))
+
